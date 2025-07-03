c7b48c6034ef40061cc19607672f8d27c105aab4
Kacper Wach <kwach@aptean.com>
2025-07-03 17:19:45 +0200

    Add background shadow when RulesLayout is visible

--- src/components/RulesLayout/RulesLayout.jsx
+++ src/components/RulesLayout/RulesLayout.jsx
@@ -1,13 +1,13 @@
-import styles from './RulesLayout.module.css';
-import IMG from '../../assets/image-rules-bonus.svg';
-import CLOSE_MARK from '../../assets/icon-close.svg';
+import styles from "./RulesLayout.module.css";
+import IMG from "../../assets/image-rules-bonus.svg";
+import CLOSE_MARK from "../../assets/icon-close.svg";
 
 export const RulesLayout = ({ areRulesShown, toggleRules }) => {
-	return (
-		<div className={`${areRulesShown ? styles.rulesLayout : styles.hidden}`}>
-			<h2>Rules</h2>
-			<img src={IMG} alt='rules sketch' />
-			<img onClick={toggleRules} src={CLOSE_MARK} alt='' />
-		</div>
-	);
+  return (
+    <div className={`${areRulesShown ? styles.rulesLayout : styles.hidden}`}>
+      <h2>Rules</h2>
+      <img src={IMG} alt="rules sketch" />
+      <img onClick={toggleRules} src={CLOSE_MARK} alt="" />
+    </div>
+  );
 };
